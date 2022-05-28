949a964b56cfa2c7fb2c28306749636db498706e
DarkTuck <98091387+DarkTuck@users.noreply.github.com>
2022-05-28 16:04:59 +0200

    działą licznik zdrowia praca nad respawnem piłki

--- Assets/scripts/dethwall.cs
+++ Assets/scripts/dethwall.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public class dethwall : MonoBehaviour
 {
+    [SerializeField] private int takenDamage;
+    [SerializeField] private HPMG _HPMG;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "ball")
         {
             ball Ball = collision.GetComponent<ball>();
             BallsManager.Instance.Balls.Remove(Ball);
+            Damage();
             Ball.Die();
         }
+    }
+    void Damage()
+    {
+        _HPMG.HP = _HPMG.HP - takenDamage;
+        _HPMG.UpdateHealth();
     }
 }

b7a9970aec9c8d8f816d84aba59c10f98b4f35db
DarkTuck <98091387+DarkTuck@users.noreply.github.com>
2022-05-28 15:16:00 +0200

    dodano graficzny licznik życia (praca nad skryptem liczenia ich)

---
+++ Assets/scripts/HPMG.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPMG : MonoBehaviour
+{
+    public int HP;
+    [SerializeField] private Image[] hearts;
+
+    private void Start()
+    {
+            UpdateHealth();
+    }
+    public void UpdateHealth()
+    {
+     for (int i = 0; i < hearts.Length; i++)
+     {
+       if (i<HP)
+       {
+           hearts[i].color = Color.gray;
+       }
+       else
+       {
+           hearts[i].color  = Color.black;
+       }
+     }   
+    }
+}

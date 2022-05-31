883c2eb473efd78270234cf649bd9a586f397ada
DarkTuck <98091387+DarkTuck@users.noreply.github.com>
2022-05-31 14:58:39 +0200

    naprawiono piłkę

--- Assets/scripts/ball.cs
+++ Assets/scripts/ball.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 public class ball : MonoBehaviour
 {
     public static event Action<ball> OBD;
+    public float thrust;
     public void Die()
     {
         OBD?.Invoke(this);
         Destroy(gameObject);
+    }
+    void FixedUpdate()
+    {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(this.transform.forward);
     }
 }
